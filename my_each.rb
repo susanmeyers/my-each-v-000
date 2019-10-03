b7956774f00bb5541955806c2f1209cbc468c41e
Susan Meyers <smmeyers24@gmail.com>
2019-10-03 02:48:40 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
-  # code here
+require 'pry'
+def my_each(words)
+  i = 0
+  while i < words.length
+    i = i + 1
+      do |word|
+       word
+      yield
+  end
+  end
 end
+
+
+# def hello_t(array)
+#   i = 0
+ 
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end
